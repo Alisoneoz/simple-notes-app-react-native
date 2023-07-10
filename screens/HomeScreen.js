d53de431d10fc660f61b20432810942e13e90884
alisoneoz <alisonestephanyoz@gmail.com>
2023-07-09 23:11:56 -0400

    Add style to HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,58 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Pressable>
+        <View style={styles.itemButton}>
+          <Text style={styles.title}>ADD NEW NOTE</Text>
+          <Icon name="arrow-forward-circle-outline" style={styles.icon}/>
+        </View>
+      </Pressable>
+
+      <Pressable>
+        <View style={styles.itemButton}>
+          <Text style={styles.title}>VIEW NOTES</Text>
+          <Icon name="arrow-forward-circle-outline" style={styles.icon}/>
+        </View>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  itemButton: {
+    width: Dimensions.get("window").width - 100,
+    height: "50%",
+    marginVertical: 30,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+    alignItems: "center",
+    elevation:50,
+    shadowOffset:{width:2,height:10},
+    shadowColor:"black",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    
+  },
+  title:{
+    fontSize:35,
+    fontWeight:"bold",
+    textAlign:"center",
+
+  },
+  icon:{
+    fontSize:50,
+    paddingTop:20,
+  }
+});
